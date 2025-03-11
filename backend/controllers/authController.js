c595dfdd3635ff64185816bd749dd2906242d5e5
EpicFailure-afk <as3704921@gmail.com>
2025-03-11 06:01:42 +0200

    update

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,19 +1,19 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import User from '../models/User';
+import User from '../models/User.js';
 
 export const registerUser = async(req, res) => {
     const {username, email, password} = req.body;
     try{
         const existingUser = await User.findOne({ email });
-        if (existingUser) return req.status(400).json({ message: 'User already exisits' });
+        if (existingUser) return res.status(400).json({ message: 'User already exisits' });
 
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = await User.create({username, email, password: hashedPassword});
 
-        req.status(201).json({ message: 'User registered successfully' });
+        res.status(201).json({ message: 'User registered successfully' });
     } catch(error){
-        req.status(500).json({ message: 'Server Error' });
+        res.status(500).json({ message: 'Server Error' });
     }
 }
 
@@ -26,7 +26,7 @@ export const loginUser = async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) return res.status(400).json({ message: 'invalid' });
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: 'lh' });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
         res.json({ token, user: { id: user._id, username: user.username, email: user.email } });
     } catch(error){
